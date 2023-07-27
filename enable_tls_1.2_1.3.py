7bbfaa302b218c7bf954eaa4c432151ee5962f73
Harsh Bhavsar <124553731+harshb-icpl@users.noreply.github.com>
2023-07-27 19:23:23 +0530

    Update enable_tls_1.2_1.3.py

--- enable_tls_1.2_1.3.py
+++ enable_tls_1.2_1.3.py
@@ -2,5 +2,4 @@ AdminTask.listSSLConfigs('[-all true -displayObjectName true]')
 
 AdminTask.modifySSLConfig('[-alias NodeDefaultSSLSettings -scopeName (cell):DESKTOP-U93A7BSNode01Cell:(node):DESKTOP-U93A7BSNode01 -keyStoreName NodeDefaultKeyStore -keyStoreScopeName (cell):DESKTOP-U93A7BSNode01Cell:(node):DESKTOP-U93A7BSNode01 -trustStoreName NodeDefaultTrustStore -trustStoreScopeName (cell):DESKTOP-U93A7BSNode01Cell:(node):DESKTOP-U93A7BSNode01 -jsseProvider IBMJSSE2 -sslProtocol TLSv1.3,TLSv1.2 -clientAuthentication false -clientAuthenticationSupported false -securityLevel HIGH -enabledCiphers ]')
 
-# Save the changes to the configuration repository
 AdminConfig.save()
